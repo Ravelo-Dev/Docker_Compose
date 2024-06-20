8ec04183a14a13142e128c6818a087704df6eae9
Ravelo-Dev <zmarcos140@gmail.com>
2024-06-20 14:52:02 -0400

    Subiendo A github

---
+++ src/index.php
@@ -0,0 +1,15 @@
+<?php
+$servername = "db";
+$username = "testuser";
+$password = "testpassword";
+$dbname = "testdb";
+
+// Crear conexión
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Verificar conexión
+if ($conn->connect_error) {
+    die("Conexión fallida: " . $conn->connect_error);
+}
+echo "Hola Mundo";
+?>
